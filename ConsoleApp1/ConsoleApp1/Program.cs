6a667aa9500536f5ade9f8c5adea32132aa3b370
kamilcyan <kamil.lucjanek@wp.pl>
2020-01-27 20:54:34 +0100

    wer

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var cities = new List<string>();
+            //cities.Add("London");
+            //cities.Add("Warsaw");
+            //cities.Add("Paris");
+
+            //Console.WriteLine("Initial List");
+            //foreach(string city in cities)
+            //{
+            //    Console.WriteLine($"  {city}");
+            //}
+
+            //Console.WriteLine($"The first city is {cities[0]}");
+            //Console.WriteLine($"The last city is {cities[cities.Count-1]}");
+
+            //cities.Insert(0, "Sydney");
+            //Console.WriteLine("After adding sydney at position 0: ");
+            //foreach (string city in cities)
+            //{
+            //    Console.WriteLine($"  {city}");
+            //}
+
+            //cities.RemoveAt(1);
+            //cities.Remove("Paris");
+            //Console.WriteLine("After removing: ");
+            //foreach (string city in cities)
+            //{
+            //    Console.WriteLine($"  {city}");
+            //}
+
+            Program program = new Program();
+
+            List<int> liczby = new List<int>();
+
+            for (int i=0; i<2000; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    liczby.Add(i);
+                }
+            }
+
+            foreach (int liczba in liczby)
+            {
+                Console.WriteLine($"  {liczba}");
+            }
+
+            
+            
+
+            Console.WriteLine(program.Suma(liczby));
+            Console.ReadKey();
+        }
+
+        public int Suma(List<int> liczbies)
+        {
+            int suma = 0;
+            foreach (int liczba in liczbies)
+            {
+                if ((liczba % 10) == 0)
+                    suma += liczba;
+            }
+            return suma;
+        }
+    }
+}
